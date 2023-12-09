7f3dc0d580714841906429b046fd9bb38edc997e
raj96 <rajpeaks@gmail.com>
2023-12-09 22:48:03 +0530

    Implments maurer rose generation

---
+++ maurer-rose/maurer.js
@@ -0,0 +1,104 @@
+document.addEventListener('DOMContentLoaded', InitializeApp);
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+const biggerDim = screenHeight > screenWidth ? screenHeight : screenWidth;
+const smallerDim = screenHeight < screenWidth ? screenHeight : screenWidth;
+
+const AppState = {
+    n: 0,
+    d: 0,
+};
+
+function _(z) {
+    return document.getElementById(z);
+}
+
+function setup() {
+
+    let canvasWidth = Math.ceil(smallerDim / 1.2);
+    let canvasHeight = canvasWidth;
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent("canvas-container");
+
+    background(0);
+
+}
+
+function getCartesianPoint(n, theta) {
+    theta *= (Math.PI / 180);
+    let r = Math.sin(n * theta);
+
+    let x = r * Math.sin(theta);
+    let y = r * Math.cos(theta);
+
+    x = map(x, 1, -1, 0, width);
+    y = map(y, 1, -1, 0, height);
+
+    return { x, y };
+}
+
+function draw() {
+    if (AppState.n == 0 || AppState.d == 0) return;
+
+    background(0);
+
+    let previousPoint = getCartesianPoint(AppState.n, 0);
+    let previousOutlinePoint = getCartesianPoint(AppState.n, 0);
+
+    for (let i = 1; i < 361; i++) {
+        let p = getCartesianPoint(AppState.n, AppState.d * i);
+
+        let op = getCartesianPoint(AppState.n, i);
+
+        stroke("lime");
+        strokeWeight(0.5);
+        line(previousPoint.x, previousPoint.y, p.x, p.y);
+        stroke("red");
+        strokeWeight(3);
+        line(previousOutlinePoint.x, previousOutlinePoint.y, op.x, op.y);
+        previousPoint = p;
+        previousOutlinePoint = op;
+    }
+
+    noLoop();
+}
+
+function AttachListenersToInputs() {
+    const nEl = _("n");
+    const dEl = _("d");
+    const downloadEl = _("download");
+
+    nEl.onkeyup = (ev) => {
+        const value = Number(ev.target.value);
+        console.log(value)
+        if (isNaN(value)) {
+            value = 0;
+            alert("n should be a number");
+        }
+
+        AppState.n = value;
+        redraw();
+    };
+    nEl.onblur = nEl.onchange;
+
+    dEl.onkeyup = (ev) => {
+        const value = Number(ev.target.value);
+        if (isNaN(value)) {
+            value = 0;
+            alert("d should be a number");
+        }
+
+        AppState.d = value;
+        redraw();
+    };
+    dEl.onblur = dEl.onchange;
+
+    downloadEl.onclick = () => {
+
+    }
+}
+
+function InitializeApp() {
+    AttachListenersToInputs();
+}
